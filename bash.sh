c9a7a6fbe2e71e03f77e32f25b2a503c4c39cff9
AVINASH082201 <avinashmarisetti08@gmail.com>
2024-03-31 16:58:33 +0000

    terraform scripts file

---
+++ bash.sh
@@ -0,0 +1,6 @@
+sudo yum -y install git
+git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
+cd flight-perdiction
+sed -i '/sklearn/d' requirements.txt
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
